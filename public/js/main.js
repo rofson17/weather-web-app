ed2222c206e83db4749973abdec2d0986b314f18
Rofson Jame Abu Siam <71087344+rofson17@users.noreply.github.com>
2021-03-22 14:04:54 +0600

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -3,41 +3,50 @@ const submitBtn=document.getElementById("submit-btn");
 const cityName=document.getElementById("cityName");
 const output=document.getElementById("city_name");
 
-const temperature=document.getElementById("temp_real_val");
-const temperature_status=document.getElementById("temp_status");
-const dataHide=document.querySelector('middle_layer');
+const  temp_real_val=document.getElementById("temp_real_val");
+const temp_status=document.getElementById("temp_status");
+const datahide=document.querySelector('.middle_layer');
 
-const getInfo=(event)=>{
-  event.preventDefault();
-  // using https://openweathermap.org
-  const api_key="";
-  let cityValue=cityName.value;
+const getInfo = async(event) => {
+    event.preventDefault();
 
-  if (cityValue==="" ){
-    output.innerHTML=`Please write the name before search`;
-    dataHide.classList.add('data_hide');
-  }else{
-    try{
-      let api_url=`api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metrie&appid=${api_key}`;
-      const response=await fetch(api_url);
-      const data=await response.json();
-      const arrData=[data];
-      output.innerText=`${arrData[0].name}, ${arrData[0].sys.country}`;
-      temperature.innerText=arrData[0].main.temp'
+    let cityVal = cityName.value;
 
-      let tempMood=arrData[0].weather[0].main;
+    if(cityVal === ""){
+        cityName.innerText = `Plz write the name before search`;
+        datahide.classList.add("data_hide");
+    }else{
+        try{
+            const api_key="metric&appid=b14425a6554d189a2d7dc18a8e7d7263";
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=${api_key}`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const arrData = [data];
 
-      if (tempMood=='Clear') temperature_status.innerText=`<i class="fas fa-sun" style="color: #eccc68;"></i>`;
-      else if (tempMood=='Clouds') temperature_status.innerText=`<i class="fas fa-cloud" style="color: #1f2f6;"></i>`;
-      else if (tempMood=='Rain') temperature_status.innerText=`<i class="fas fa-cloud-rain" style="color: #a4b0be;"></i>`;
-      else (tempMood=='sun') temperature_status.innerText=`<i class="fas fa-sun" style="color: #eccc68;"></i>`;
-      dataHide.classList.remove('data_hide');
-    }catch{
-      output.innerText=`Please enter the city name properly`;
-      dataHide.classList.add('data_hide');
+            output.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+            temp_real_val.innerText = arrData[0].main.temp;
+            const tempMood = arrData[0].weather[0].main;
+
+            //condition to check sunny or cloudy
+            if (tempMood == "Clear") temp_status.innerHTML =`<i class='fas  fa-sun' style='color: #eccc68;'></i>`;
+            else if (tempMood == "Clouds")  temp_status.innerHTML=`<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>`;
+            else if (tempMood == "Rain") temp_status.innerHTML =` <i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>`;
+            else temp_status.innerHTML =`<i class='fas  fa-cloud' style='color:#f1f2f6;'></i>`;
+
+
+            datahide.classList.remove('data_hide');
+            cityVal = "";
+            
+        }catch{
+            cityVal = " ";
+            datahide.classList.add("data_hide");
+            cityName.innerText =  `please enter the proper city name`;
+            console.log('please add the proper city name');
+        }
+
     }
-  }
 }
+
 submitBtn.addEventListener('click', getInfo);
 
 // current time
